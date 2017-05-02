be2d286d9a6736337eecd64ac22694e91c82dc65
kiven <kiven.mr@gmail.com>
2017-05-02 13:42:21 +0800

    Do not import on top level

--- walila/__init__.py
+++ walila/__init__.py
@@ -4,9 +4,5 @@
   Walila - Awesome toolkit for dobechina internal.
 """
 
-
-from .config import load_app_config  # noqa
-from .message import MessageProducer, MessageConsumer  # noqa
-
 version_info = (0, 1, 0)
 __version__ = ".".join([str(v) for v in version_info])
