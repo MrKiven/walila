7a661ab6c95031e40cbe3592729f3a83c7dde88d
kiven <kiven.mr@gmail.com>
2017-05-04 14:32:17 +0800

    Add walila logger handler

--- walila/__init__.py
+++ walila/__init__.py
@@ -4,5 +4,17 @@
   Walila - Awesome toolkit for dobechina internal.
 """
 
+import logging
+
+logger = logging.getLogger(__name__)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(
+    logging.Formatter("[Walila %(levelname)-7s] %(message)s")
+)
+logger.addHandler(console)
+logger.setLevel(logging.INFO)
+
+
 version_info = (0, 3, 2)
 __version__ = ".".join([str(v) for v in version_info])
